f7c943504c32786e689c7961979f3a52f3706d6a
divyaB32 <divyaboomibalakan@gmail.com>
2026-03-10 10:45:38 +0530

    Initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from "path";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
-import contactRoutes from "./routes/contactRoutes.js"; // ✅ ADD THIS
+import contactRoutes from "./routes/contactRoutes.js";
 import matchRoutes from "./routes/matchRoutes.js";
 
 dotenv.config();
@@ -13,24 +13,16 @@ connectDB();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ================= STATIC FILE SERVING =================
 app.use("/uploads", express.static("uploads"));
-app.use("/hangover", express.static(path.join(process.cwd(), "../frontend/public/hangover")));
-app.use("/preview", express.static(path.join(process.cwd(), "../frontend/public/preview")));
-app.use("/tiles", express.static(path.join(process.cwd(), "../frontend/public/tiles")));
-// ======================================================
 
-// routes
 app.use("/api/products", productRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/contact", contactRoutes); // ✅ REGISTER CONTACT ROUTES
+app.use("/api/contact", contactRoutes);
 app.use("/api/match-tiles", matchRoutes);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
